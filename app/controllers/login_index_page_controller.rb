c9a6c297b7a9d4ff95d58f6d0bfe920ce96814c6
ansonmela <anson.melakayil@yahoo.com>
2018-02-15 00:26:46 -0500

    added login on home page, need to figure out where to go after hitting sign in

--- app/controllers/login_index_page_controller.rb
+++ app/controllers/login_index_page_controller.rb
@@ -1,4 +1,10 @@
 class LoginIndexPageController < ApplicationController
 	def index
+		@user = User.all
+	end
+
+	def create
+		@user = User.create
+		redirect_to root_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-	root 'login_index_page#index'
+  	devise_for :users
+		root :to => 'login_index_page#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
